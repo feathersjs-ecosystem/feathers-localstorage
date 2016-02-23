ff254c9d3a16266b156b38f583cf074a9ffa70f8
Eric Kryski <e.kryski@gmail.com>
2016-02-23 10:51:36 -0700

    adding instructions on how to use and updatingn example

--- example/app.js
+++ example/app.js
@@ -2,24 +2,24 @@ var feathers = require('feathers');
 var bodyParser = require('body-parser');
 var rest = require('feathers-rest');
 var socketio = require('feathers-socketio');
-var service = require('../lib');
-var localstorage = require('localstorage-memory');
+var localstorage = require('../lib');
+var storage = require('localstorage-memory');
 
 // Create a feathers instance.
 const app = feathers()
   // Enable REST services
   .configure(rest())
-  // Enable REST services
+  // Enable Socket.io services
   .configure(socketio())
   // Turn on JSON parser for REST services
   .use(bodyParser.json())
   // Turn on URL-encoded parser for REST services
   .use(bodyParser.urlencoded({ extended: true }));
 
-// Create an in-memory Feathers service with a default page size of 2 items
+// Create an in-memory localstorage Feathers service with a default page size of 2 items
 // and a maximum size of 4
-app.use('/messages', service({
-  storage: localstorage,
+app.use('/messages', localstorage({
+  storage: storage,
   paginate: {
     default: 2,
     max: 4
