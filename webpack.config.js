a48fe957317b54fc2f82c7f4415171d2e63cf6a4
David Luecke <daff@neyeon.com>
2018-03-07 09:43:04 -0800

    Finalize Webpack update

--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,46 @@
 const path = require('path');
 const webpack = require('webpack');
-const merge = require('webpack-merge');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
-function createConfig (name, isProduction = false) {
-  const output = name === 'index' ? 'feathers-localstorage' : name;
-  const commons = {
-    entry: `./lib/${name}.js`,
-    output: {
-      library: ['feathers', 'localstorage'],
-      libraryTarget: 'umd',
-      path: path.resolve(__dirname, 'dist'),
-      filename: `${output}.js`
-    },
-    module: {
-      rules: [{
-        test: /\.js/,
-        exclude: /node_modules\/(?!(@feathersjs))/,
-        loader: 'babel-loader'
-      }]
-    }
-  };
+const isProduction = process.env.NODE_ENV === 'production';
+const output = isProduction ? 'feathers-localstorage.min.js' : 'feathers-localstorage.js';
 
-  const dev = {
+const config = {
+  entry: `./lib`,
+  output: {
+    library: ['feathers', 'localstorage'],
+    libraryTarget: 'umd',
+    path: path.resolve(__dirname, 'dist'),
+    filename: `${output}`
+  },
+  module: {
+    rules: [{
+      test: /\.js/,
+      exclude: /node_modules\/(?!(@feathersjs))/,
+      loader: 'babel-loader'
+    }]
+  },
+  plugins: []
+};
+
+if (!isProduction) {
+  Object.assign(config, {
+    mode: 'development',
     devtool: 'source-map'
-  };
-
-  const production = {
-    devtool: 'cheap-module-source-map',
-    output: {
-      filename: `${output}.min.js`
-    },
-    plugins: [
-      new UglifyJSPlugin({
-        uglifyOptions: {
-          ie8: false,
-          comments: false,
-          sourceMap: false
-        }
-      }),
-      new webpack.DefinePlugin({
-        'process.env.NODE_ENV': JSON.stringify('production')
-      })
-    ]
-  };
-
-  return merge(commons, isProduction ? production : dev);
+  });
+} else {
+  Object.assign(config, {
+    mode: 'production',
+    plugins: [new UglifyJSPlugin({
+      uglifyOptions: {
+        ie8: false,
+        comments: false,
+        sourceMap: false
+      }
+    }), new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    })]
+  });
 }
 
-module.exports = [
-  createConfig('index'),
-  createConfig('index', true)
-];
+module.exports = config;
